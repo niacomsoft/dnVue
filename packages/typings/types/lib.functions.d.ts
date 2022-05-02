846b97652e512646c83dced8b2ba2b0efbd26a7e
王 玉才 <netowls-studio@outlook.com>
2022-05-02 16:00:27 +0800

    feat(Issue #2): 新增有返回值的方法定义。

--- packages/typings/types/lib.functions.d.ts
+++ packages/typings/types/lib.functions.d.ts
@@ -76,4 +76,84 @@ declare namespace dnvue {
          */
         (arg: TArgument): Promise<void>;
     }
+
+    /**
+     * 定义了 TReturn 类型返回值的方法。
+     *
+     * @interface Func
+     * @template TReturn 返回值类型。
+     * @function
+     * @return {TReturn}
+     */
+    interface Func<TReturn> {
+        /**
+         * 定义了 TReturn 类型返回值的方法。
+         *
+         * @returns {TReturn}
+         * @function
+         */
+        (): TReturn;
+    }
+
+    /**
+     * 定义了 TReturn 类型返回值的方法。
+     *
+     * @interface ParameterizedFunc
+     * @template TArgument 参数类型。
+     * @template TReturn 返回值类型。
+     * @function
+     */
+    interface ParameterizedFunc<TArgument, TReturn> {
+        /**
+         * 定义了 TReturn 类型返回值的方法。
+         *
+         * @param {TArgument} arg 参数值。
+         * @returns {TReturn}
+         * @function
+         */
+        (arg: TArgument): TReturn;
+    }
+
+    /**
+     * (异步的方法) 定义了 TReturn 类型返回值的方法。
+     *
+     * @interface AsyncFunc
+     * @template TReturn 返回值类型。
+     * @function
+     * @return {Promise<TReturn>}
+     * @async
+     */
+    interface AsyncFunc<TReturn> {
+        /**
+         * (异步的方法) 定义了 TReturn 类型返回值的方法。
+         *
+         * @returns {Promise<TReturn>}
+         * @function
+         * @async
+         */
+        (): Promise<TReturn>;
+    }
+
+    /**
+     * (异步的方法) 定义了 TReturn 类型返回值的方法。
+     *
+     * @interface ParameterizedAsyncFunc
+     * @template TArgument 参数类型。
+     * @template TReturn 返回值类型。
+     * @function
+     * @return {Promise<TReturn>}
+     * @async
+     */
+    interface ParameterizedAsyncFunc<TArgument, TReturn> {
+        /**
+         * (异步的方法) 定义了 TReturn 类型返回值的方法。
+         *
+         * @param {TArgument} arg 参数值。
+         * @returns {Promise<TReturn>}
+         * @function
+         * @return {Promise<TReturn>}
+         * @async
+         */
+        (arg: TArgument): Promise<TReturn>;
+    }
 }

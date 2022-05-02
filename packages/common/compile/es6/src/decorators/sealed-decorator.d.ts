a03c4bd25c559370991ea45df629cf02a8406704
王 玉才 <netowls-studio@outlook.com>
2022-05-02 17:13:11 +0800

    feat(Issue #3): 新增密闭类装饰器。

---
+++ packages/common/compile/es6/src/decorators/sealed-decorator.d.ts
@@ -0,0 +1,7 @@
+/**
+ * 提供了用于修饰密闭类相关的方法。
+ *
+ * @export
+ * @param {Function} constructor 用于修改的构造方法。
+ */
+export declare function sealed(constructor: Function): void;
--- packages/common/compile/es6/src/decorators/sealed-decorator.js
+++ packages/common/compile/es6/src/decorators/sealed-decorator.js
@@ -2,4 +2,13 @@
 // COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.
 // LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
 // **************************************************************************************************************************
-export { sealed } from "./src";
+/**
+ * 提供了用于修饰密闭类相关的方法。
+ *
+ * @export
+ * @param {Function} constructor 用于修改的构造方法。
+ */
+export function sealed(constructor) {
+    Object.seal(constructor);
+    Object.seal(constructor.prototype);
+}

ebe1de4a95f896d214f972adf0de292a684da01f
王 玉才 <netowls-studio@outlook.com>
2022-05-14 10:04:57 +0800

    feat(Issue #1): 新增公共组件。

---
+++ packages/app/src/components/abstracts/component-base.vue
@@ -0,0 +1,14 @@
+<!-- 
+  **************************************************************************************************************************
+  COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.
+  LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+  **************************************************************************************************************************
+-->
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "vComponentBase",
+});
+</script>

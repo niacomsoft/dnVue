1f744acf4c0dd0856bf8c7dbf0f5eff76f21c2c0
王 玉才 <netowls-studio@outlook.com>
2022-05-25 12:35:14 +0800

    feat(Issue #11): 新增 HTTP 核心项目。

---
+++ packages/http/core/compile/es6/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// **************************************************************************************************************************
+// COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.
+// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+// **************************************************************************************************************************

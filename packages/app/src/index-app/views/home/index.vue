677b1da24ffa4d7b40b73e1a5852f13d52a8be53
王 玉才 <netowls-studio@outlook.com>
2022-05-27 13:12:46 +0800

    feat(Issue #13 #1): 新增应用清单组件 v-appmanifest。

--- packages/app/src/index-app/views/home/index.vue
+++ packages/app/src/index-app/views/home/index.vue
@@ -1,19 +1,20 @@
 <!-- 
   **************************************************************************************************************************
   COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.
-  LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+  LICENSED UNDER THE MIT LICENSE. SEE LICENSE FI】LE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
   **************************************************************************************************************************
 -->
 
 <template>
   <v-flexbox direction="column">
     <v-appbar :logo-image-url="readonlyLogoImage" :title="$t('APPLICATION_NAME')"></v-appbar>
+    <v-app-manifest :title="$t('APPLICATION_MANIFEST')" />
   </v-flexbox>
 </template>
 
 <script lang="ts" setup>
 import { computed } from "vue";
-import { vFlexbox, vAppbar } from "../../../components";
+import { vFlexbox, vAppbar, vAppManifest } from "../../../components";
 
 /**
  * 获取一个字符串，用于表示 LOGO 图片 URL。

921b170f40d95a6a136700ade56fad89dfbd494d
王 玉才 <netowls-studio@outlook.com>
2022-05-27 13:13:33 +0800

    feat(Issue #13 #1): 新增应用清单抽屉组件 v-appmanifest。

--- packages/app/src/components/v-appmanifest/v-appmanifest.vue
+++ packages/app/src/components/v-appmanifest/v-appmanifest.vue
@@ -6,5 +6,50 @@
 -->
 
 <template>
-    <div></div>
+  <el-drawer v-model="showAppManifestDrawer" :show-close="false" direction="ltr" size="350px" custom-class="v-appmanifest-drawer" modal-class="v-appmanifest-drawer-modal">
+    <template #title>
+      <div data-v-role="header">
+        <div><v-icon color="var(--md-grey-900)" size="var(--v-text-size--xl)" name="mdiApps" @click="onDrawerIconClick" /></div>
+        <div>
+          <span>{{ $t("APPLICATION_MANIFEST") }}</span>
+        </div>
+      </div>
+    </template>
+  </el-drawer>
 </template>
+
+<script lang="ts" setup>
+import { useComponentStateStore } from "../../lib";
+import { storeToRefs } from "pinia";
+import { DnvueComponentProps } from "../component-props";
+import { vIcon } from "../v-icon";
+
+/**
+ * 组件状态。
+ */
+const componentState = useComponentStateStore();
+
+/**
+ * 可监控的组件状态。
+ */
+const { showAppManifestDrawer } = storeToRefs(componentState);
+
+const props = defineProps(
+  Object.assign({}, DnvueComponentProps, {
+    /**
+     * 设置或获取一个字符串，用于表示应用标题。
+     */
+    title: {
+      type: String,
+      default: null,
+    },
+  })
+);
+
+/**
+ * 用于处理抽屉图标单击事件。
+ */
+function onDrawerIconClick() {
+  componentState.toggleAppManifestDrawerVisible(false);
+}
+</script>
